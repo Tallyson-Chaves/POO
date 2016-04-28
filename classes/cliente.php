8da5a6e1b137a6b323647e9d4a638132f8d17517
Tallyson Chaves <tallyson.adm@gmail.com>
2016-04-28 12:01:06 -0300

    Modificações Sintaxe

--- classes/cliente.php
+++ classes/cliente.php
@@ -1,14 +1,26 @@
 <?php
-
+require_once 'database/dados.php';
 
 class Cliente {
     
     public $dados;
     
     public function dados ($dados){
-        echo    'Nome: '.$dados[0].'<br/>'.
-                'CPF: '.$dados[1].'<br/>'.
-                'Endere&ccedilo: '.$dados[2].'<br/>'.
-                'Telefone: '.$dados[3];
+        $i=0;
+        
+        while($i <= 9){
+        echo    
+                '-----------------------------------------------------'.'<br/>'.
+                'ID: '.'000'.$i.'<br/>'.
+                'Nome: '.$dados[$i][0].'<br/>'.
+                'CPF: '.$dados[$i][1].'<br/>'.
+                'Endere&ccedilo: '.$dados[$i][2].'<br/>'.
+                'Telefone: '.$dados[$i][3].'<br/>'.
+                '-----------------------------------------------------'.'<br/>';
+                $i++;
+        }
+        
     }
+    
+    
 }
--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 require_once 'classes/cliente.php';
 
-$dados = array(
-    0=>"Tallyson",
-    1=>"111.824.656-09",
-    2=>"Teste",
-    3=>"Teste"
-);
-
-$cliente1 = new Cliente();
-
-echo $cliente1->dados($dados);
+ $cliente1 = new Cliente();
+ echo $cliente1->dados($dados);
